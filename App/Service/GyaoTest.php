d4c4c9855bb89eeefe5648be0c5dfc4f6469f5df
gongyao <gongyao1992@qq.com>
2022-04-13 15:15:53 +0800

    [提交代码]

--- App/Service/GyaoTest.php
+++ App/Service/GyaoTest.php
@@ -1,9 +1,11 @@
 <?php
-namespace App\Service;
+namespace Gongyao1992\ComposerTest\Service;
 
+// 对 命名空间的 理解
 class GyaoTest
 {
 //"Gongyao1992\\ComposerTest\\": "App/"
+
     public function getName()
     {
         return "gongyao";
